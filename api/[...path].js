9df01a953edd95b1b03deacd7fb562b853fae4b8
enderduque1758-png <enderduque1758@gmail.com>
2026-09-13 21:03:32 -0500

    Consolidate legacy API routes into one Vercel catch-all function

---
+++ api/[...path].js
@@ -0,0 +1,21 @@
+import handler from '../server/handler.js';
+
+export default function(req,res){
+  if(req.method==='OPTIONS'){
+    const origin=String(req.headers&&req.headers.origin||'');
+    if(origin)res.setHeader('Access-Control-Allow-Origin',origin);
+    res.setHeader('Vary','Origin');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,X-Odds-Api-Key');
+    return res.status(204).end();
+  }
+
+  let path=req.query&&req.query.path;
+  if(Array.isArray(path))path=path.join('/');
+  if(!path){
+    const url=String(req.url||'').split('?')[0];
+    path=url.replace(/^\/api\/?/,'');
+  }
+  req.query={...(req.query||{}),path:String(path||'').replace(/^\/+|\/+$/g,'')};
+  return handler(req,res);
+}
